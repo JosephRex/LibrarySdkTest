8a4d3cba56a436b75cd9b8770178b752d68bb17a
JosephRex <josephrex.trinitymascot@gmail.com>
2019-05-17 12:37:56 +0530

    First Commit and Push

---
+++ testmodule/src/main/java/com/tmcsrex/testmodule/Tmcsrexxutils.java
@@ -0,0 +1,13 @@
+package com.tmcsrex.testmodule;
+
+import android.util.Log;
+
+public class Tmcsrexxutils {
+
+    public static final String TAG = "RexTestLibrary: ";
+
+
+    public static void display(String msg){
+        Log.d(TAG, msg);
+    }
+}
